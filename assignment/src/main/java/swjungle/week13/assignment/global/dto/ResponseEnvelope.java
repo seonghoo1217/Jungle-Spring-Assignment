c5dae4eeba3bdebc1bd04ab3d07b21404fc4c31d
seonghoo1217 <lsh6451217@gmail.com>
2024-06-05 17:13:00 +0900

    #3 feat: 봉투 패턴(Envelope Pattern) 전역 DTO 개발

---
+++ assignment/src/main/java/swjungle/week13/assignment/global/dto/ResponseEnvelope.java
@@ -0,0 +1,11 @@
+package swjungle.week13.assignment.global.dto;
+
+public record ResponseEnvelope<T>(String code, T data, String message) {
+    public static <T> ResponseEnvelope<T> of(T data) {
+        return new ResponseEnvelope<>(null, data, null);
+    }
+
+    public static <T> ResponseEnvelope<T> of(String code, T data, String message) {
+        return new ResponseEnvelope<>(code, data, message);
+    }
+}
