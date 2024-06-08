f110a99e30cfd83b70577f7dfa5fa79d2b2c489e
seonghoo1217 <lsh6451217@gmail.com>
2024-06-09 01:39:14 +0900

    #7 feat: 게시물 찾지 못할경우 예외 추가

--- assignment/src/main/java/swjungle/week13/assignment/global/exception/ErrorCode.java
+++ assignment/src/main/java/swjungle/week13/assignment/global/exception/ErrorCode.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import swjungle.week13.assignment.domain.exception.ArticleNotFoundException;
 import swjungle.week13.assignment.domain.exception.MemberNotFoundException;
 import swjungle.week13.assignment.domain.exception.PasswordNotMatchException;
 import swjungle.week13.assignment.domain.exception.UsernameExistException;
@@ -22,7 +23,8 @@ public enum ErrorCode {
 
     USERNAME_EXIST(HttpStatus.BAD_REQUEST, "중복된 username 입니다.", Set.of(UsernameExistException.class)),
     NOTFOUND_MEMBER(HttpStatus.BAD_REQUEST, "회원을 찾지 못하였습니다.", Set.of(MemberNotFoundException.class)),
-    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.", Set.of(PasswordNotMatchException.class));
+    PASSWORD_NOT_MATCH(HttpStatus.BAD_REQUEST, "비밀번호가 일치하지 않습니다.", Set.of(PasswordNotMatchException.class)),
+    NOTFOUND_ARTICLE(HttpStatus.BAD_REQUEST, "게시물을 찾지 못하였습니다.", Set.of(ArticleNotFoundException.class));
     private final HttpStatusCode status;
     private final String code;
     private final String message;
